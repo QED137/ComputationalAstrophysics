a7e2fafce8a489adb907862e0342ac6c060bcc2f
Janmajay Kumar <106250110+QED137@users.noreply.github.com>
2025-11-24 16:47:13 +0100

    plotting the result from teukolosky solution

--- plottingTeukolosky.py
+++ plottingTeukolosky.py
@@ -1,1 +1,13 @@
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv("frame_200.csv")
+Nr = 250
+Nth = 180
+
+psi = df['re'].values.reshape(Nr, Nth)
+plt.imshow(psi, aspect='auto', cmap='inferno')
+plt.colorbar()
+plt.show()
